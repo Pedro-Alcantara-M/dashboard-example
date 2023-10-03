7eeb17ff87c5c2c32572272ee87cc3117808e610
Pedro Alcântara <pedrinrn@gmail.com>
2023-10-03 14:53:03 -0300

    base vit config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'dashboard-example',
+  base: '/dashboard-example',
   plugins: [react(), tsconfigPaths(), svgr()],
 })
